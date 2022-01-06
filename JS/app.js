29a5094cb7aabfda6cf38fb3a8a00b3d245837ce
Jakub <czaplickijakub1@gmail.com>
2022-01-06 13:02:10 -0500

    Fixed JS

--- JS/app.js
+++ JS/app.js
@@ -1,36 +1,69 @@
 const powerNums = document.querySelector('#powerContainer');
-const amtWhite = 5;
-const whiteBalls = 70;
-const powerBall = 27;
+const amtWhite = 5; //amount of white balls
+const whiteBalls = 69; //max number you can choose
+const powerBall = 26; //max number you can choose
+const api_url = 'https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8&size=6';
 
 const content = document.createElement('div');
-let h1 = document.createElement('h1')
-generateNum();
+content.classList.add('numbers');
+powerNums.appendChild(content);
 
-powerNums.appendChild(h1);
+generate();
 
-function generateNum()
+function generate()
 {
-    fetchAPI();
+    numStorage = ['one', 'two', 'three', 'four', 'five']
+    for(let i = 0; i < amtWhite; i++)
+    {
+        display(numStorage[i])
+    }
+    displayPower('six');
 }
 
-function fetchAPI()
+async function display(name1)
 {
-    let JSONrandoNum;
-    let randoNum;
-    let temp;
-    fetch('https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8&size=6')
-        .then(response => response.json())
-        .then(data => JSONrandoNum = data)
-        .then(() =>  randoNum = JSONrandoNum.data[0])
-        .then(() => {
-            if(randoNum > 69)
-            {
-                h1.textContent = 'error'
-            }
-            else
-            {
-                h1.textContent = randoNum
-            }
-         })
+    let name = name1;
+    name = document.createElement('h1');
+    content.appendChild(name);
+    let temp = await getWhite();
+    if((temp > whiteBalls) || temp == 0)
+    {
+        while((temp > whiteBalls) || temp == 0)
+        {
+            temp = await getWhite();
+        }
+    }
+    name.innerText = temp;
+}
+
+async function displayPower(name1)
+{
+    let name = name1;
+    name = document.createElement('h1');
+    content.appendChild(name);
+    let temp = await getWhite();
+    if((temp > powerBall) || temp == 0)
+    {
+        while((temp > powerBall) || temp == 0)
+        {
+            temp = await getWhite();
+        }
+    }
+    name.innerText = 'power: ' + temp;
+}
+
+async function getWhite()
+{
+    const response = await fetch(api_url);
+    const numResponse = await response.json();
+    const numGenerated = numResponse.data[0];
+    return numGenerated;
+}
+
+async function getPower()
+{
+    const response = await fetch(api_url);
+    const numResponse = await response.json();
+    const numGenerated = numResponse.data[0];
+    return numGenerated;
 }
