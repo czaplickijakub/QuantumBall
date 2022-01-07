4b42bed7da32f155e87f1a8f20611d6b6ce89d7f
Jakub <czaplickijakub1@gmail.com>
2022-01-07 15:07:02 -0500

    Basic Functionality

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,43 @@
 const powerNums = document.querySelector('#powerContainer');
+const oneText = document.querySelector('.oneText');
 const whiteBalls = 69; //max number you can choose
 const powerBall = 26; //max number you can choose
 const api_url = 'https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8&size=6';
 
-const content = document.createElement('div'); //create a div with a class 'numbers'
-content.classList.add('numbers');
+const content = document.createElement('div'); //create a div...
+content.classList.add('numbers'); //with a class 'numbers'
 powerNums.appendChild(content); //add as child node
 
 function generate()
 {
+    //resetText();
+    /*
+    for(let i = 0; i < 5; i++)
+    {
+        const content = document.createElement('div'); //create a div...
+        content.classList.add('set' + i); //with a class 'numbers'
+        powerNums.appendChild(content); //add as child node
+    }
+    */
+
     numStorage = ['one', 'two', 'three', 'four', 'five']
     for(let i = 0; i < numStorage.length; i++)
     {
         display(numStorage[i])
     }
     displayPower('six');
+    oneText.innerText = 'Your lucky numbers are:';
+}
+
+function resetText()
+{
+    document.getElementById("oneText").reset();  
 }
 
 async function display(name1)
 {
     let name = name1;
-    name = document.createElement('h1');
+    name = document.createElement('p');
     content.appendChild(name);
     let temp = await getNum();
     if((temp > whiteBalls) || temp == 0)
@@ -36,7 +53,7 @@ async function display(name1)
 async function displayPower(name1)
 {
     let name = name1;
-    name = document.createElement('h1');
+    name = document.createElement('p1');
     content.appendChild(name);
     let temp = await getNum();
     if((temp > powerBall) || temp == 0)
@@ -46,7 +63,7 @@ async function displayPower(name1)
             temp = await getNum();
         }
     }
-    name.innerText = 'power: ' + temp;
+    name.innerText = temp;
 }
 
 async function getNum() //fetch 
