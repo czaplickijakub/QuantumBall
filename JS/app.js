c5b0610d6e1a3363f29d09858fa25b7aa60b8137
Jakub <czaplickijakub1@gmail.com>
2022-01-07 15:46:46 -0500

    Temp JS

--- JS/app.js
+++ JS/app.js
@@ -4,28 +4,27 @@ const whiteBalls = 69; //max number you can choose
 const powerBall = 26; //max number you can choose
 const api_url = 'https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8&size=6';
 
-const content = document.createElement('div'); //create a div...
-content.classList.add('numbers'); //with a class 'numbers'
-powerNums.appendChild(content); //add as child node
-
 function generate()
 {
     //resetText();
-    /*
+
+/*
+    for(let i = 0; i < 2; i++)
+    {
+        let name = i.toString();
+    }
+*/
+
+
+    const content = document.createElement('div'); //create a div...
+    content.classList.add('numbers'); //with a class 'numbers'
+    powerNums.appendChild(content); //add as child node
+
     for(let i = 0; i < 5; i++)
     {
-        const content = document.createElement('div'); //create a div...
-        content.classList.add('set' + i); //with a class 'numbers'
-        powerNums.appendChild(content); //add as child node
+        display(content);
     }
-    */
-
-    numStorage = ['one', 'two', 'three', 'four', 'five']
-    for(let i = 0; i < numStorage.length; i++)
-    {
-        display(numStorage[i])
-    }
-    displayPower('six');
+    displayPower(content);
     oneText.innerText = 'Your lucky numbers are:';
 }
 
@@ -34,10 +33,9 @@ function resetText()
     document.getElementById("oneText").reset();  
 }
 
-async function display(name1)
+async function display(content)
 {
-    let name = name1;
-    name = document.createElement('p');
+    let name = document.createElement('p');
     content.appendChild(name);
     let temp = await getNum();
     if((temp > whiteBalls) || temp == 0)
@@ -50,10 +48,9 @@ async function display(name1)
     name.innerText = temp;
 }
 
-async function displayPower(name1)
+async function displayPower(content)
 {
-    let name = name1;
-    name = document.createElement('p1');
+    let name = document.createElement('p1');
     content.appendChild(name);
     let temp = await getNum();
     if((temp > powerBall) || temp == 0)
