ec70828bd16e21a57fa74db80629793177a0fbe9
Jakub <czaplickijakub1@gmail.com>
2022-01-08 10:43:59 -0500

    Polished JS

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,14 @@
-const powerNums = document.querySelector('#powerContainer');
-const oneText = document.querySelector('.oneText');
-const whiteBalls = 69; //max number you can choose
-const powerBall = 26; //max number you can choose
-const api_url = 'https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8&size=6';
+const powerNums = document.querySelector('#powerContainer'); //Selecting DOM to manipulate
+const oneText = document.querySelector('.oneText'); //Selecting DOM to manipulate
 
-let amtToGen;
-let content;
-let clicked = false;
+let amtToGen; //keeps track of amount of sets requested
+let clicked = false; //functionality to lock buttons
 
 function generate(num)
 {
-    if(!clicked)
+    if(!clicked) //only can click once
     {
+        let content;
         amtToGen = num;
         clicked = true;
 
@@ -22,13 +19,14 @@ function generate(num)
             content.classList.add('numbers' + i); //with a class 'numbers'
             powerNums.appendChild(content); //add as child node
 
+            oneText.innerText = 'Your lucky numbers are:';
+
             for(let j = 0; j < 5; j++)
             {
                 display(content);
             }
         displayPower(content);
         }
-        oneText.innerText = 'Your lucky numbers are:';
     }
 }
 
@@ -44,6 +42,8 @@ function resetText()
 
 async function display(content)
 {
+    const whiteBalls = 69; //max number you can choose
+
     let name = document.createElement('p');
     content.appendChild(name);
     let temp = await getNum();
@@ -59,6 +59,8 @@ async function display(content)
 
 async function displayPower(content)
 {
+    const powerBall = 26; //max number you can choose
+
     let name = document.createElement('p1');
     content.appendChild(name);
     let temp = await getNum();
@@ -74,6 +76,8 @@ async function displayPower(content)
 
 async function getNum() //fetch 
 {
+    const api_url = 'https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8&size=6';
+
     const response = await fetch(api_url);
     const numResponse = await response.json();
     const numGenerated = numResponse.data[0];
